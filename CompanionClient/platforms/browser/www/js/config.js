4f03d60d7281f1d3f15302616e5e0cf28989359b
viloshnaS <39422333+viloshnaS@users.noreply.github.com>
2018-11-06 10:59:32 +0100

    Initialisation

---
+++ CompanionClient/platforms/browser/www/js/config.js
@@ -0,0 +1,4 @@
+var URL ="http://localhost:80/Gamification/server";
+
+var months = [ "Jan", "Feb", "Mar", "Apr", "May", "June",
+    "July", "Aug", "Sep", "Oct", "Nov", "Dec" ];
---
+++ CompanionClient/www/js/config.js
@@ -0,0 +1,1 @@
+var URL ="http://localhost:80/CodeReviewCompanion/CompanionClient";
