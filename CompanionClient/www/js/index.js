934a655b9c12bdd26f4e8e0524ad39fd4e7eb907
viloshnaS <39422333+viloshnaS@users.noreply.github.com>
2018-11-06 15:47:59 +0100

    user login page

--- CompanionClient/www/js/index.js
+++ CompanionClient/www/js/index.js
@@ -18,7 +18,51 @@ function displayProjectList(results){
   var result_arr = JSON.parse(results); // converting results to JSON object
       display_string ="";
       result_arr.forEach(function(project) {
-            display_string += " <option id='"+project.project_id+"'>"+project.project_name+"</option>";
+            display_string += " <option value='"+project.project_id+"'>"+project.project_name+"</option>";
           });
          $("#projectList").html(display_string);
 }
+
+function getUserList(projectId){
+
+  $.ajax({
+    url : URL+'/getUserList.php', 
+    type : 'GET', 
+    data: { project_id:projectId } ,
+    crossDomain: true, 
+    success : function(response){ 
+
+      displayUserList(response);
+    }, error : function(resultat, statut, erreur){
+  
+      console.log("Error encountered. Could not retrieve details");
+    }
+  });
+};
+
+
+function displayUserList(results){
+
+  var users = JSON.parse(results); // converting results to JSON object
+      display_string ="<table>";
+
+      var len = users.length
+
+       for (i = 0; i <len; i+=3) { 
+          for(j=i;j<i+3;j++){
+            if(j >=len){
+              break;
+            }
+            display_string = display_string+"<tr>";
+            display_string = display_string+"<td> <img src='img/"+users[j].photo+"' alt='"+users[j].first_name+"'/> </td>";
+            display_string = display_string +"</tr>";
+          }
+      }
+          
+        
+      display_string +="</table>";
+
+      alert(display_string);
+         $("#UserList").html(display_string);
+}
+
